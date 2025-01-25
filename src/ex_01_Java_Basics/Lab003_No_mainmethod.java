ac1ec27aacb2daa8af2aa0bb955957fbf5658ec6
Sabarikanth93 <sabarikanthmba@gmail.com>
2025-01-25 08:58:24 +0100

    Week 1 task finished

---
+++ src/ex_01_Java_Basics/Lab003_No_mainmethod.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class Lab003_No_mainmethod {
+
+    //    if there is no main methode the code will compile and we cant execute/run the code
+
+}
---
+++ src/ex_01_Java_Basics/_123.java
@@ -0,0 +1,5 @@
+package ex_01_Java_Basics;
+
+public class _123 {
+//    we can use _ and numbers as a class name
+}
