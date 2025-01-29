1e253d7bb5c3cd7a469cb4fa5bfadef5ac8f7dc9
sabarikandasamy <sabarikanthmba@gmail.com>
2025-01-29 18:44:42 +0100

    11th Jan class exercises.

---
+++ src/Ex_02_Java_Basics/Lab014_Multiple_main_method.java
@@ -0,0 +1,10 @@
+package Ex_02_Java_Basics;
+
+public class Lab014_Multiple_main_method {
+    public static void main(String[] args) {
+        System.out.println("QA Engineer");
+    }
+public void main(String args) {
+//        can run many main functions but not with the same
+}
+}
