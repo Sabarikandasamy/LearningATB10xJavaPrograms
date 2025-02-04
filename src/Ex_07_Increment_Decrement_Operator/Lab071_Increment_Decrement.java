7ba904cc5a985c21a4fb1d8e84f8693d3c336ba3
sabarikandasamy <sabarikanthmba@gmail.com>
2025-02-04 08:05:23 +0100

    19th Jan class.

--- src/Ex_07_Increment_Decrement_Operator/Lab071_Increment_Decrement.java
+++ src/Ex_07_Increment_Decrement_Operator/Lab071_Increment_Decrement.java
@@ -1,6 +1,6 @@
 package Ex_07_Increment_Decrement_Operator;
 
-public class lab071_Increment_Decrement {
+public class Lab071_Increment_Decrement {
     public static void main(String[] args) {
         int a = 10;
         System.out.println(++a); // Output is 11
