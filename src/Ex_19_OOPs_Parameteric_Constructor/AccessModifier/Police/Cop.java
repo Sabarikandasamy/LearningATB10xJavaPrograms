a5254a03f59bfd75fd1f2117edb0ccdd8b604fea
sabarikandasamy <sabarikanthmba@gmail.com>
2025-03-02 17:34:50 +0100

    1March finished

--- src/Ex_19_OOPs_Parameteric_Constructor/AccessModifier/Police/Cop.java
+++ src/Ex_19_OOPs_Parameteric_Constructor/AccessModifier/Police/Cop.java
@@ -1,15 +1,14 @@
 package Ex_19_OOPs_Parameteric_Constructor.AccessModifier.Police;
 
-public static void main(String[] args) {
-
-}
 public class Cop {
     public int gun;
-    String icard;
+    String iCard;
 
-    public Cop(int gun);
-    this.gun = gun;
+    public Cop(int gun) {
+        this.gun = gun;
+    }
+    protected void canIShoot(){
+        System.out.println("Yes You can shoot!");
+    }
 }
-  public void canIshoot(){
-      System.out.println("Yes, You can shoot!!");
-  }
+
